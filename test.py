98b1abdfa798c111dc8ee7081e1c36f46ea3aca8
washirving <anonim359@gmail.com>
2022-11-01 01:41:32 +0300

    Update test.py

--- test.py
+++ test.py
@@ -2,7 +2,7 @@
 from http.server import HTTPServer
 import os
 
-# Get port number from the PORT environment varaible or 3000 if not specified
+# Get port nummber from the PORT environment varaible or 3000 if not specified
 port = os.getenv('PORT', 3000)
 
 server = HTTPServer(('0.0.0.0', port), MyServer)
