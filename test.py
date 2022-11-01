1eee74e232787d531afbba20b40da92ac54c1399
washirving <anonim359@gmail.com>
2022-11-02 02:02:32 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,1 @@
-
-from http.server import HTTPServer
-import os
-
-# Get port nummber from the PORT environment varaible or 3000 if not specified
-port = os.getenv('PORT', 3000)
-
-server = HTTPServer(('0.0.0.0', port), MyServer)
-server.serve_forever()
+print("hello world")
